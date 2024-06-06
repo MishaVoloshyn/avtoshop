0e7b19df6f928f85ac1cb4940352a7dcf346af4a
MISHA <vol.mykhailo@gmail.com>
2024-06-06 18:39:03 +0300

    start Header-light.js

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,10 +1,13 @@
 import { Outlet  } from 'react-router-dom';
 import {CarsList} from "../Components/Cars-list-1";
 import {Brands} from "../Components/brands";
+import {HeaderLight} from "../Components/Header-light";
 
 export const MainPage = () => (
     <>
         <p>Hello MainPage!</p>
+
+        <HeaderLight/>
         <CarsList/>
         <Brands/>
         <Outlet/>
