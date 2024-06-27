3a797ac725aa207672df4230728e076387cf18cd
dimathecreator <dzhosan777@gmail.com>
2024-06-27 13:16:30 +0200

    added usInfo

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -2,9 +2,18 @@ import { Outlet  } from 'react-router-dom';
 import {FAQSection} from "../Components/FAQ";
 import {HeaderDark} from "../Components/Header-dark-1";
 import {Footer} from "../Components/Footer";
+import {Benefits1} from "../Components/benefits-1";
+import {SliderVideo} from "../Components/Slider-video";
+import {Brands} from "../Components/Brands";
+import {Team} from "../Components/Team";
+
 export const AboutUs = () => (
     <>
         <HeaderDark/>
+        <Benefits1/>
+        <SliderVideo/>
+        <Brands/>
+        <Team/>
         <FAQSection/>
         <Outlet/>
         <Footer/>
