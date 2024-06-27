682b332fcb3f6a7456de405202bd87577094dd8a
dimathecreator <dzhosan777@gmail.com>
2024-06-27 16:47:16 +0200

    added BlogCarInfo

--- src/Pages/blog-page.js
+++ src/Pages/blog-page.js
@@ -1,10 +1,12 @@
 import { Outlet  } from 'react-router-dom';
 import {Footer} from "../Components/Footer";
+import {BlogCarInfo} from "../Components/blog-car-info";
 
 export const BlogPage = () => (
     <>
         <p>Hello Blog-Page!</p>
         <Outlet/>
+        <BlogCarInfo/>
         <Footer/>
     </>
 
