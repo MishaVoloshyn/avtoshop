24919dca1d590cb531e2d2a1e2c80b69c33e95ce
dimathecreator <dzhosan777@gmail.com>
2024-06-28 02:31:50 +0200

    added reviews

--- src/Pages/Car-card.js
+++ src/Pages/Car-card.js
@@ -3,11 +3,13 @@ import {HeaderDark} from "../Components/Header-dark-1";
 import {Footer} from "../Components/Footer";
 import {CarInfo} from "../Components/Card-info";
 import React from "react";
+import {AddReview} from "../Components/add-review";
 
 export const CarCard = () => (
     <>
         <HeaderDark/>
         <CarInfo/>
+        <AddReview/>
         <Footer/>
         <Outlet/>
     </>
